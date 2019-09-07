2b46e62f4bc63ff9428ac1bee88e68dc663282f5
basilcea <ogbonna.basil3@gmmail.com>
2019-09-07 19:15:04 +0100

    initialize use of gatsby

---
+++ src/components/header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default  () => {
+    return (<div>
+    <div>
+    <img src='' alt=''/>
+    <p>PreciousO</p>
+    </div>
+    <div>
+    <socialIcon></socialIcon>
+    <socialIcon></socialIcon>
+    <button>Contact Me</button>
+    </div>
+    </div>)
+}
