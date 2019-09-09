e3e66d41f325d49485985db0af74aec8bbd604de
basilcea <ogbonna.basil3@gmmail.com>
2019-09-09 07:37:45 +0100

    create header for mobile

--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React from "react"
+import icon from "../assests/icon.png"
+import headerStyles from "./header.module.css"
+import {
+  FaFacebookF,
+  FaTwitter,
+  FaLinkedinIn,
+  FaBars,
+  FaTimes
+} from "react-icons/fa"
 
-export default  () => {
-    return (<div>
-    <div>
-    <img src='' alt=''/>
-    <p>PreciousO</p>
+export default () => {
+  return (
+    <div className={headerStyles.headerContainer}>
+      <div className={headerStyles.topMenu}>
+        <div className={headerStyles.iconDiv}>
+          <img className={headerStyles.icon} src={`${icon}`} alt="" />
+          <p className={headerStyles.title}>PreciousO</p>
+        </div>
+        <FaBars className={headerStyles.hamburger} />
+        <FaTimes className={headerStyles.hamburger} />
+      </div>
+      <div className={headerStyles.socials}>
+        <div className={headerStyles.socialsIcon}>
+          {" "}
+          <FaFacebookF className={headerStyles.singleSocialIcon}/>
+          <FaTwitter className={headerStyles.singleSocialIcon} />
+          <FaLinkedinIn className={headerStyles.singleSocialIcon}/>
+        </div>
+
+        <button className={headerStyles.button}>Contact Me</button>
+      </div>
     </div>
-    <div>
-    <socialIcon></socialIcon>
-    <socialIcon></socialIcon>
-    <button>Contact Me</button>
-    </div>
-    </div>)
+  )
 }
